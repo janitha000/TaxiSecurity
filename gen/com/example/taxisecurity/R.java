7ca52b0740558d4fc0c807acbe9ad88abef6ade6
Pasadi <pasadi.munasinghe@gmail.com>
2014-05-01 12:58:52 +0530

    allocator updated

--- gen/com/example/taxisecurity/R.java
+++ gen/com/example/taxisecurity/R.java
@@ -226,32 +226,42 @@ containing a value of this type.
         public static final int Button02=0x7f060008;
         public static final int Button03=0x7f060009;
         public static final int Button04=0x7f06000a;
-        public static final int action_about=0x7f06001d;
-        public static final int action_settings=0x7f06001c;
-        public static final int autoComplete=0x7f060010;
+        public static final int LinearLayout1=0x7f06000f;
+        public static final int action_about=0x7f060027;
+        public static final int action_settings=0x7f060026;
+        public static final int autoComplete=0x7f06001a;
+        public static final int btnExtendOK=0x7f060013;
+        public static final int btnExtendTime=0x7f060018;
+        public static final int btnStartTime=0x7f060012;
+        public static final int btnStopTime=0x7f060014;
         public static final int button1=0x7f06000b;
         public static final int button2=0x7f06000c;
         public static final int button3=0x7f06000d;
-        public static final int button_register=0x7f060017;
+        public static final int button_register=0x7f060021;
         public static final int container=0x7f060006;
-        public static final int editText_mail=0x7f060014;
-        public static final int editText_name=0x7f060012;
-        public static final int editText_password=0x7f060016;
+        public static final int editText_mail=0x7f06001e;
+        public static final int editText_name=0x7f06001c;
+        public static final int editText_password=0x7f060020;
+        public static final int edtHours=0x7f060010;
+        public static final int edtMinutes=0x7f060011;
         public static final int hybrid=0x7f060004;
+        public static final int layoutRemaining=0x7f060015;
         public static final int map=0x7f06000e;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
         public static final int satellite=0x7f060002;
-        public static final int second_text_email=0x7f06001a;
-        public static final int second_text_name=0x7f060019;
-        public static final int second_text_pass=0x7f06001b;
-        public static final int tab=0x7f060018;
+        public static final int second_text_email=0x7f060024;
+        public static final int second_text_name=0x7f060023;
+        public static final int second_text_pass=0x7f060025;
+        public static final int tab=0x7f060022;
         public static final int terrain=0x7f060003;
-        public static final int text1=0x7f06000f;
+        public static final int text1=0x7f060019;
+        public static final int textRemaining=0x7f060016;
         public static final int textView1=0x7f060005;
-        public static final int textView2=0x7f060013;
-        public static final int textView3=0x7f060015;
-        public static final int textview_Register=0x7f060011;
+        public static final int textView2=0x7f06001d;
+        public static final int textView3=0x7f06001f;
+        public static final int textview_Register=0x7f06001b;
+        public static final int tvTimeCount=0x7f060017;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f080000;
@@ -260,11 +270,12 @@ containing a value of this type.
         public static final int activity_about=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_map=0x7f030002;
-        public static final int fragment_main=0x7f030003;
-        public static final int list_item=0x7f030004;
-        public static final int loginform=0x7f030005;
-        public static final int policedetail=0x7f030006;
-        public static final int second=0x7f030007;
+        public static final int activity_timer=0x7f030003;
+        public static final int fragment_main=0x7f030004;
+        public static final int list_item=0x7f030005;
+        public static final int loginform=0x7f030006;
+        public static final int policedetail=0x7f030007;
+        public static final int second=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f0b0000;
@@ -407,6 +418,8 @@ containing a value of this type.
          */
         public static final int AppTheme=0x7f0a0001;
         public static final int Theme_Transparent=0x7f0a0002;
+        public static final int blinkText=0x7f0a0004;
+        public static final int normalText=0x7f0a0003;
     }
     public static final class xml {
         public static final int activity_averse=0x7f040000;
--- src/com/example/taxisecurity/timeAllocatorActivity.java
+++ src/com/example/taxisecurity/timeAllocatorActivity.java
@@ -1,7 +1,186 @@
 package com.example.taxisecurity;
 
 import android.app.Activity;
+import android.R.layout;
+import android.app.Activity;
+import android.app.PendingIntent.CanceledException;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class timeAllocatorActivity extends Activity {
+public class timeAllocatorActivity extends Activity implements View.OnClickListener {
 
+	private Button buttonStartTime, buttonStopTime, buttonExtend;
+	private EditText edtHours, edtMinutes;
+	private LinearLayout layoutRemaining;   	 
+	private CountDownTimer countDownTimer; 
+	private TextView textViewRemaining;	
+	private TextView textViewShowTime;
+	
+	private long totalTimeCountInMilliseconds; 
+	private long timeRemaining;										
+	private long timeBlinkInMilliseconds;
+	private boolean blink; 
+	private boolean error=false;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timer);
+
+		buttonStartTime = (Button) findViewById(R.id.btnStartTime);
+		buttonStopTime = (Button) findViewById(R.id.btnStopTime);
+		buttonExtend = (Button) findViewById(R.id.btnExtendTime);
+		textViewShowTime = (TextView) findViewById(R.id.tvTimeCount);
+		edtMinutes = (EditText) findViewById(R.id.edtMinutes);
+		edtHours = (EditText) findViewById(R.id.edtHours);
+		layoutRemaining = (LinearLayout) findViewById(R.id.layoutRemaining);
+
+		buttonStartTime.setOnClickListener(this);
+		buttonStopTime.setOnClickListener(this);
+		buttonExtend.setOnClickListener(this);
+
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.btnStartTime) {
+			textViewShowTime.setTextAppearance(getApplicationContext(),
+					R.style.normalText);
+			
+			setTimer();
+			
+			buttonStopTime.setVisibility(View.VISIBLE);
+			layoutRemaining.setVisibility(View.VISIBLE);
+			buttonStartTime.setVisibility(View.GONE);
+			edtMinutes.setVisibility(View.GONE);
+			edtHours.setVisibility(View.GONE);
+			edtMinutes.setText("");
+			edtHours.setText("");
+			
+			startTimer();
+
+		} else if (v.getId() == R.id.btnStopTime) {
+			countDownTimer.cancel();
+			
+			buttonStartTime.setVisibility(View.VISIBLE);
+			buttonStopTime.setVisibility(View.GONE);
+			layoutRemaining.setVisibility(View.GONE);
+			edtMinutes.setVisibility(View.VISIBLE);
+			edtHours.setVisibility(View.VISIBLE);
+
+		}  else if (v.getId() == R.id.btnExtendTime) {
+			countDownTimer.cancel();
+			buttonExtend.setVisibility(View.GONE);	
+			update();
+			buttonStopTime.setVisibility(View.VISIBLE);	
+			Toast.makeText(timeAllocatorActivity.this, "Allocated time is extended by 10 mins....",
+			Toast.LENGTH_LONG).show();
+
+		} 
+	}
+	private void update(){
+			countDownTimer = new CountDownTimer(timeRemaining+(1*60*1000), 500){
+			
+			@Override
+			public void onTick(long leftTimeInMilliseconds) {
+				textViewShowTime.setTextAppearance(getApplicationContext(),
+						R.style.normalText);
+				textViewShowTime.setVisibility(View.VISIBLE);
+				long seconds = leftTimeInMilliseconds / 1000;
+				if (leftTimeInMilliseconds < timeBlinkInMilliseconds) {	
+					
+					textViewShowTime.setTextAppearance(getApplicationContext(),
+							R.style.blinkText);
+					buttonExtend.setVisibility(View.VISIBLE);
+					if (blink) {
+						textViewShowTime.setVisibility(View.VISIBLE);
+					} else {
+						textViewShowTime.setVisibility(View.INVISIBLE);
+					}
+					blink = !blink; // toggle the value of blink
+				}
+				
+				textViewShowTime.setText(String.format("%02d", seconds / (60*60))
+						+ ":" + String.format("%02d", (seconds % (60*60))/60)	
+						+ ":" + String.format("%02d", seconds % 60));				
+			}
+
+				@Override
+				public void onFinish() {
+				
+				}		
+		}.start();
+	}
+	private void setTimer() {
+		int hours = 0;
+		int minutes= 0;
+		int time= 0;
+		
+		if (!edtHours.getText().toString().equals("") && !edtMinutes.getText().toString().equals("") ) {
+			minutes = Integer.parseInt(edtMinutes.getText().toString());
+			hours = Integer.parseInt(edtHours.getText().toString());
+			time =(60*hours) + minutes;
+		}else if(edtHours.getText().toString().equals("") && !edtMinutes.getText().toString().equals("")){
+			minutes = Integer.parseInt(edtMinutes.getText().toString());
+			time=minutes;
+		}else if(!edtHours.getText().toString().equals("") && edtMinutes.getText().toString().equals("")){
+			hours = Integer.parseInt(edtHours.getText().toString());
+			time=60*hours;
+		} 
+		else{
+				error=true;
+				Toast.makeText(timeAllocatorActivity.this, "Please Enter Travel Time...",
+				Toast.LENGTH_LONG).show();						
+		}
+		
+		totalTimeCountInMilliseconds = 60 * time * 1000;
+		timeBlinkInMilliseconds = 50 * 1000;
+	}
+
+	private void startTimer() {
+		countDownTimer = new CountDownTimer(totalTimeCountInMilliseconds, 500) {
+			@Override
+			public void onTick(long leftTimeInMilliseconds) {
+				timeRemaining =leftTimeInMilliseconds;
+				long seconds = leftTimeInMilliseconds / 1000;
+				if (leftTimeInMilliseconds < timeBlinkInMilliseconds) {
+					textViewShowTime.setTextAppearance(getApplicationContext(),
+							R.style.blinkText);
+					buttonExtend.setVisibility(View.VISIBLE);
+					if (blink) {
+						textViewShowTime.setVisibility(View.VISIBLE);
+					} else {
+						textViewShowTime.setVisibility(View.INVISIBLE);
+					}
+					blink = !blink; 
+				}
+				textViewShowTime.setText(String.format("%02d", seconds / (60*60))
+						+ ":" + String.format("%02d", (seconds % (60*60))/60)	
+						+ ":" + String.format("%02d", seconds % 60));            
+			}
+			@Override
+			public void onFinish() {
+				if(totalTimeCountInMilliseconds!=0)
+					textViewShowTime.setText("Time up!");
+				else 
+					textViewShowTime.setText("");
+					textViewRemaining.setVisibility(View.VISIBLE);
+					textViewShowTime.setVisibility(View.VISIBLE);
+					buttonStartTime.setVisibility(View.VISIBLE);
+					buttonStopTime.setVisibility(View.GONE);
+					edtMinutes.setVisibility(View.VISIBLE);
+					edtHours.setVisibility(View.VISIBLE);
+				}
+		}.start();
+
+	}
 }
