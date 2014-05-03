91f2b6b5ac0d0da8cdeba1f645006035d2ba5cba
vindyahettige <vindihh@gmail.com>
2014-05-03 11:55:07 +0530

    commit before

---
+++ src/com/example/taxisecurity/Place.java
@@ -0,0 +1,12 @@
+package com.example.taxisecurity;
+
+public class Place {
+
+	public String reference;
+	public String name;
+	public String icon;
+	public String formatted_address;
+	public String formatted_phone_number;
+	//public String reference;
+	
+}
