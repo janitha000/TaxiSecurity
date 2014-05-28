e2c5b4a20d796545e49e665e6dbc46ae2e50d29d
ysperera <ysperera@yahoo.com>
2014-05-29 05:22:21 +0530

    settings completed

--- src/com/example/taxisecurity/Settings.java
+++ src/com/example/taxisecurity/Settings.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Settings extends Activity {
 	
@@ -26,33 +27,34 @@ public class Settings extends Activity {
 	     contact2name = (EditText) findViewById(R.id.contact2name);
 		 contact2no = (EditText) findViewById(R.id.contact2no);
 		 
-		 SharedPreferences preferences = getSharedPreferences("ContactData", 0);
+		SharedPreferences preferences = getSharedPreferences("ContactData", 0);
 			contact1name.setText(preferences.getString("chosen1Name"," "));
 			contact1no.setText(preferences.getString("chosen1No", " "));
-			contact2name.setText(preferences.getString("chosen2Name", " "));
+		    contact2name.setText(preferences.getString("chosen2Name", " "));
 			contact2no.setText(preferences.getString("chosen2No", " "));
 		
-		//Button b = (Button) findViewById(R.id.button1);
-		//b.setOnClickListener(new OnClickListener() {
+		Button b = (Button) findViewById(R.id.button1);
+		b.setOnClickListener(new OnClickListener() {
 			
-			//@Override
-			//public void onClick(View v) {
-				// contact1name = (EditText) findViewById(R.id.contact1name);
-				 //contact1no = (EditText) findViewById(R.id.contact1no);
-			     //contact2name = (EditText) findViewById(R.id.contact2name);
-				// contact2no = (EditText) findViewById(R.id.contact2no);
+			@Override
+			public void onClick(View v) {
+				 contact1name = (EditText) findViewById(R.id.contact1name);
+				 contact1no = (EditText) findViewById(R.id.contact1no);
+			     contact2name = (EditText) findViewById(R.id.contact2name);
+				 contact2no = (EditText) findViewById(R.id.contact2no);
 				
 				
 				
-				//SharedPreferences preferences = getSharedPreferences("ContactData", 0);
+				
+				// SharedPreferences preferences = getSharedPreferences("ContactData", 0);
 				//contact1name.setText(preferences.getString("chosen1Name"," "));
 				//contact1no.setText(preferences.getString("chosen1No", " "));
 				//contact2name.setText(preferences.getString("chosen2Name", " "));
 				//contact2no.setText(preferences.getString("chosen2No", " "));
-			//}
+			}
 			
 			
-		//});
+		});
 	}
 	  
 	
@@ -68,6 +70,8 @@ public class Settings extends Activity {
 		editor.putString("chosen2Name",contact2name.getText().toString());
 		editor.putString("chosen2No",contact2no.getText().toString());
 		
+		
+		Toast.makeText(Settings.this, "Saved", Toast.LENGTH_LONG).show();
 		editor.commit();
 	}
 
